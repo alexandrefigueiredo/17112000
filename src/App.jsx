6adf731c021c71054c9c8646c9f7c6ac2c243375
Alexandre Figueiredo <ale.fig1701@gmail.com>
2025-02-25 23:49:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import dezesseteonzedoismil from '/17112000.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <img src={dezesseteonzedoismil} className="logo" alt="17112000" />
+        <h1>17112000</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
